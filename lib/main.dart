6f34cc5ffd6d84145d6faa6b1b5d4fbe5c3382bb
Manuel Moscoso <mmoscoso@utalca.cl>
2026-08-21 10:18:09 -0400

    Updated with the first version of the table

--- lib/main.dart
+++ lib/main.dart
@@ -4,118 +4,146 @@ void main() {
   runApp(const MyApp());
 }
 
+/// Estados de la celda en el tablero.
+enum CellType {
+  voidCell,    // Fuera de límites jugables (Esquinas 2x2)
+  emptyHole,   // Casilla jugable desocupada
+  occupiedPeg, // Casilla jugable con clavija presente
+}
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+   @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     title: 'Solitario Ingles',
+     theme: ThemeData(primarySwatch: Colors.blue),
+     home: const PegSolitaireScreen(), // Apuntamos a nuestra nueva pantalla
+   );
+ }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class PegSolitaireScreen extends StatelessWidget {
+const PegSolitaireScreen({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  static const int gridSize = 7;
+  static const int totalCells = gridSize * gridSize; // 49 casillas
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  /// Determina el tipo de celda según sus coordenadas matriciales (row, col)
+  CellType _getCellType(int row, int col) {
+    // Esquinas 2x2 no jugables en el tablero inglés estándar
+    final bool isCorner = (row < 2 || row > 4) && (col < 2 || col > 4);
+    if (isCorner) {
+      return CellType.voidCell;
+    }
+    // El resto de las 32 posiciones inician ocupadas
+    return CellType.occupiedPeg;
+  }
 
-  final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+ Widget _gameBoard() {
+   return Center(
+     child: Padding(
+       padding: const EdgeInsets.all(8.0),
+       child: AspectRatio(
+         aspectRatio: 1.0, // Cuadrado perfecto
+         child: GridView.builder(
+           physics: const NeverScrollableScrollPhysics(), // Bloquea el scroll
+           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+             crossAxisCount: gridSize, // 7 columnas
+             crossAxisSpacing: 2.0,
+             mainAxisSpacing: 2.0,
+           ),
+           itemCount: totalCells, // 7x7 = 49 celdas
+           itemBuilder: (context, index) {
+             // Convertir el índice en coordenadas matriciales
+             final int row = index ~/ gridSize;
+             final int col = index % gridSize;
+              final CellType cellType = _getCellType(row, col);
+             return PegCell(row: row, col: col, cellType: cellType);
+           },
+         ),
+       ),
+     ),
+   );
+ }
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: const Text('Solitario')),
+    body: SafeArea( // Protege la UI de los bordes del dispositivo
+       child: Column( // Apila el marcador arriba y el tablero abajo
+         children: [
+           // Área de Status
+           Container(
+             height: 60,
+             color: Colors.grey[300],
+             child: const Center(
+               child: Text('STATUS: 349 segundos | Piezas restantes: 33',
+                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+               ),
+             ),
+           ),
+           const Divider(height: 1),
+           // Área de Juego
+           Expanded( // Expande el tablero para llenar la pantalla
+             child: _gameBoard(),
+           ),
+         ],
+       ),
+     ),
+   );
+ }
+
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class PegCell extends StatelessWidget {
+  final int row;
+  final int col;
+  final CellType cellType;
+  final bool isSelected;
+  final VoidCallback? onTap;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const PegCell({
+    super.key,
+    required this.row,
+    required this.col,
+    required this.cellType,
+    this.isSelected = false,
+    this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[400],
+        border: Border.all(color: Colors.grey[600]!, width: 1.5),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      child: Center(
+        child: cellType == CellType.occupiedPeg
+            ? Container(
+                width: 30,
+                height: 30,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                ),
+              )
+            : cellType == CellType.emptyHole
+                ? Container(
+                    width: 30,
+                    height: 30,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                  )
+                : null, // No dibuja nada para voidCell
       ),
     );
   }
